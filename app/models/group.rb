16e422b154a2a0cf2df11240750d977137e442d5
Ajinkya <ajinkyakathe013@gmail.com>
2017-07-06 00:03:02 +0530

    Changing Split logic

--- app/models/group.rb
+++ app/models/group.rb
@@ -16,29 +16,53 @@ class Group < ActiveRecord::Base
 
 	# Get equal share
 	def split_total_amt(current_user)
-		result_array = [];
 		equal_share = (get_total_expense / get_no_of_users).round(2) rescue 0
-		user_share = current_user.user_expense(self.id)
-		my_remaining_share = equal_share - user_share
-		all_users = self.users - [current_user]
-		type = my_remaining_share > 0 ? 'pay him' : 'owes you'
-		result_array = find_user_split_amt(all_users, equal_share, my_remaining_share, type, result_array)
+		#all_users = self.users - [current_user]
+		all_users = self.users
+		result_array = find_user_split_amt(all_users, equal_share, current_user)
 		return result_array
 	end
 
 	# Find split for each user in group
-	def find_user_split_amt(all_users, equal_share ,my_remaining_share, type, result_array)
-			all_users.each do |user|
-				remaining_amt = equal_share - user.user_expense(self.id)
-					if remaining_amt > 0
-						if my_remaining_share.abs >= remaining_amt
-							result_array.push([user.email, remaining_amt.abs ,type] )
-						else
-							result_array.push([user.email ,my_remaining_share.abs, type] )
-						end
-					end
+	def find_user_split_amt(all_users, equal_share , current_user)
+		result_array = [];
+		tmp_array = all_users.map{|user| [user.email, equal_share - user.user_expense(self.id)]}
+		p_array = tmp_array.select{|email, v| v > 0 }
+		n_array = tmp_array - p_array
+		#n_array = tmp_array.select{|email, v| v < 0 }
+
+		p_array.each_with_index do |p_record, p_index|
+			n_array.each_with_index do |n_record, n_index|
+				if n_record[1].abs > p_record[1]
+					puts "#{p_record[0]} will pay to #{n_record[0]} value = #{p_record[1]}"
+					result_array = create_result_set(p_record, n_record, current_user, result_array, p_record[1])
+					n_value = n_record[1].abs - p_record[1]
+					n_array[n_index] = [n_record[0], n_value]
+					n_record = [n_record[0], n_value]
+					p_array[p_index] = [p_record[0], 0]
+					p_record = [p_record[0], 0]
+				else
+					puts "#{p_record[0]} will pay to #{n_record[0]} value = #{n_record[1].abs}"
+					result_array = create_result_set(p_record, n_record, current_user, result_array, n_record[1].abs)
+					value = p_record[1] - n_record[1].abs
+					p_array[p_index] = [p_record[0], value]
+					p_record = [p_record[0], value]
+					n_record = [n_record[0], 0]
+					n_array[n_index] = [n_record[0], 0]
+				end
 			end
+		end
+		return result_array
+	end
+
+	def create_result_set(p_record, n_record, current_user, result_array, replace_value)
+		if p_record[0] == current_user.email && replace_value > 0
+			result_array.push([n_record[0], replace_value, 'pay him'])
+		elsif n_record[0] == current_user.email && replace_value > 0
+			result_array.push([p_record[0], replace_value, 'owes you'])
+		end
 		result_array
 	end
+
 
 end
